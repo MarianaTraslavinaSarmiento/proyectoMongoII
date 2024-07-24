6da01c4b9716536fa4ecedb5b5bbfccf58229d09
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-07-24 13:23:52 -0500

    feat: :sparkles: Get all available movies finished

--- src/modules/peliculas.js
+++ src/modules/peliculas.js
@@ -15,4 +15,76 @@ export class Pelicula{
         Pelicula.instancePelicula = this;
     }
 
+    async getAllAvailableMovies(){
+
+        try{
+
+            const db = await this.adminDbService.connect();
+
+            const moviesAvailable = await db.collection('peliculas').aggregate(
+                [
+                    {
+                      $lookup: {
+                        from: "proyecciones",
+                        localField: "_id",
+                        foreignField: "pelicula_id",
+                        as: "proyecciones",
+                      },
+                    },
+                  
+                    { $unwind: "$proyecciones" },
+                  
+                    {
+                      $lookup: {
+                        from: "salas",
+                        localField: "proyecciones.sala_id",
+                        foreignField: "_id",
+                        as: "proyecciones.sala",
+                      },
+                    },
+                  
+                    { $unwind: "$proyecciones.sala" },
+                  
+                    {
+                      $project: {
+                        "proyecciones.pelicula_id": 0,
+                        "proyecciones.sala_id": 0,
+                        "proyecciones.sala._id": 0,
+                        "proyecciones._id": 0,
+                        sinopsis: 0,
+                      },
+                    },
+                  
+                    {
+                      $group: {
+                        _id: "$_id", 
+                        titulo: {
+                          $first: "$titulo"
+                        },
+                        generos: {
+                          $first: "$generos"
+                        },
+                        duracion_min: {
+                          $first: "$duracion_min"
+                        },
+                              clasificacion: {
+                          $first: "$clasificacion"
+                        },
+                        proyecciones: {
+                          $push: "$proyecciones"
+                        }
+                      }
+                    }
+                ]
+            ).toArray()
+
+            return moviesAvailable;
+
+        }catch(error){
+            return {error: error.name, message: error.message}
+        }finally{
+            await this.adminDbService.close();
+        }
+    }
+
 }
