419deb3a49e043678adb37fbbcd10ae0398f00d2
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-08-11 01:56:30 -0500

    feat: :construction: Carousel of all available movies created

--- src/components/moviesCarousel.vue
+++ src/components/moviesCarousel.vue
@@ -3,23 +3,21 @@
 import { ref, onMounted } from 'vue';
 import axios from 'axios'
 
-const movie = ref([])
+const movies = ref([])
 let index = 0
 
 const moviesFetch = async()=>{
     try {
-        const res = await axios.get('');
-        movie.value = res.data;
+        const res = await axios.get('http://localhost:5001/peliculas');
+        console.log(res.data)
+        movies.value = res.data;
     } catch (error) {
         console.error(error)
     }
 }
 
 
-
-
-
-
+onMounted(moviesFetch);
 
 </script>
 
@@ -30,7 +28,16 @@ const moviesFetch = async()=>{
         <p>See all</p>  
     </div>
 
-    
+    <div class="movies__carousel">
+        <div class="movie__slider" :style="{ transform: `translateX(-${index.value * 100}%)` }">
+            <div v-for="movie in movies" :key="movie.id" class="movie__slide">
+                <img :src="movie.caratula" :alt="movie.titulo" class="movie__poster">
+                <h3 class="movie__title">{{ movie.titulo }}</h3>
+                <p class="movie__genre">{{ movie.generos.join(', ') }}</p>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <style scoped>
@@ -53,6 +60,63 @@ const moviesFetch = async()=>{
     font-weight: bold;
     font-size: 1em;
    }
+
+   .movies__carousel {
+      position: relative;
+      width: 100%;
+      height: 470px;
+    }
+
+    .movie__slider {
+      display: flex;
+      transition: transform 0.5s ease;
+      height: 480px;
+      overflow-x: scroll;
+      scroll-snap-type: x mandatory;
+      overflow-y: hidden;
+      margin-left: 15px;
+
+    }
+
+    .movie__slide {
+      padding: 15px 15px 0px 0px;
+      color: var(--color-white);
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+      max-height: 100%;
+      scroll-snap-align: center;
+      
+    }
+    .movie__poster {
+    
+      width: 270px;
+      height: 90%;
+      border-radius: 10px;
+
+    }
+
+    .movie__title {
+      margin-top: 10px;
+      font-size: 16px;
+      text-align: center;
+      width: 100%;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      max-width: 250px;
+
+    }
+
+    .movie__genre {
+      color: #666;
+      font-size: 14px;
+      text-align: center;
+      margin-top: 3px;
+    }
+
 
 
 </style>
