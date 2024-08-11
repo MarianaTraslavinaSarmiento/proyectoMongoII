938fdaefde4a2831dd6d7d82c79e19be62f9b298
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-08-10 22:47:45 -0500

    feat: :construction: carousel of the movies in progress

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { RouterLink, RouterView } from 'vue-router'
 import  HeaderUser  from '../src/components/HeaderUser.vue'
 import SearchInput from '../src/components/SearchInput.vue'
+import moviesCarousel from '../src/components/moviesCarousel.vue'
 </script>
 
 <template>
@@ -10,6 +11,7 @@ import SearchInput from '../src/components/SearchInput.vue'
 
   <HeaderUser />
   <SearchInput />
+  <moviesCarousel />
 
   </main>
 
---
+++ src/components/moviesCarousel.vue
@@ -0,0 +1,58 @@
+<script setup>
+
+import { ref, onMounted } from 'vue';
+import axios from 'axios'
+
+const movie = ref([])
+let index = 0
+
+const moviesFetch = async()=>{
+    try {
+        const res = await axios.get('');
+        movie.value = res.data;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
+
+
+
+
+
+</script>
+
+<template>
+
+    <div class="now__playing">
+        <p>Now playing</p>      
+        <p>See all</p>  
+    </div>
+
+    
+</template>
+
+<style scoped>
+
+   .now__playing {
+    padding: 5px 15px 0px 15px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+   }
+
+   .now__playing p:first-child{
+    color: var(--color-white);
+    font-weight: bold;
+    font-size: 1.2em;
+   }
+
+   .now__playing :last-child{
+    color: var(--color-red);
+    font-weight: bold;
+    font-size: 1em;
+   }
+
+
+</style>
