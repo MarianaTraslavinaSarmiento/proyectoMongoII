765f78ac97f2c12402f8a0a07ea77572327a4526
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-08-12 07:30:27 -0500

    feat: :construction: Movie card component in progress

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <div class="movie__card">
+        <div class="cover">
+            <img src="" alt="">
+        </div>
+        <div class="movie__overviewer">
+            <div>
+                <h3>Nombre de la pelicula</h3>
+                <small>genero, genero, genero</small>
+            </div>
+            <button> Watch Trailer</button>
+        </div>
+        <p>sinopsissinopsissinopsissinopsissinopsissinopsissinopsissinopsissinopsissinopsissinopsissinopsissinopsissinopsissinopsissinopsissinopsissinopsissinopsis</p>
+    </div>
+
+</template>
+
+<style scoped>
+
+.movie__card{
+    background-color: blue;
+    height: 50vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 5px;
+    width: 100%;
+}
+
+
+
+.cover {
+    width: 95%;
+    height: 60%;
+    background-color: red;
+    border: 1px solid var(--color-white);
+    border-radius: 15px;
+}
+
+.movie__overviewer{
+    display: flex;
+    width: 100%;
+    padding-inline: 15px;
+    justify-content: space-between;
+}
+
+button{
+    width: 30%;
+    height: 30px;
+    border-radius: 5px;
+    border: none
+}
+
+
+
+</style>
--- src/views/MovieInfo.vue
+++ src/views/MovieInfo.vue
@@ -1,11 +1,13 @@
 <script setup>
 
 import HeaderBack from '../components/HeaderBack.vue';
+import MovieCard from '@/components/MovieCard.vue';
 </script>
 
 <template>
 
     <HeaderBack header="Details of the movie"/>
+    <MovieCard />
 
 
 
